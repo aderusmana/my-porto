245f72a04b7eaffecd7e4008a8f4344c10f032bd
aderusmana <aderusmana777@gmail.com>
2023-10-26 16:25:04 +0700

    Add Header and Socials

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Socials from '@/components/Socials';
+
+// images
+
+// links
+
+// COMPONENT
 
 const Header = () => {
   return (
-    <div className={'absolute z-30 w-full'}>
-      <h2>Header</h2>
+    <div
+      className={
+        'absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'
+      }
+    >
+      <div className={'container mx-auto'}>
+        <div
+          className={
+            'flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8'
+          }
+        >
+          {/*logo */}
+          <Link href={'/'}>
+            <Image
+              src={'/logo2.svg'}
+              alt={'logo2'}
+              width={300}
+              height={80}
+              priority={true}
+            />
+          </Link>
+          <Socials />
+        </div>
+      </div>
     </div>
   );
 };
--- components/Socials.js
+++ components/Socials.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
+//links
+import Link from 'next/link';
+//icons
+import { RiGithubLine, RiInstagramLine, RiLinkedinLine } from 'react-icons/ri';
 const Socials = () => {
-    return (
-        <div>
-
-        </div>
-    );
+  return (
+    <div className={'flex items-center gap-x-5 text-lg'}>
+      <Link
+        href={'/youtube'}
+        className={'hover:text-accent transition-all duration-300'}
+      >
+        <RiInstagramLine />
+      </Link>
+      <Link
+        href={'/youtube'}
+        className={'hover:text-accent transition-all duration-300'}
+      >
+        <RiLinkedinLine />
+      </Link>
+      <Link
+        href={'/youtube'}
+        className={'hover:text-accent transition-all duration-300'}
+      >
+        <RiGithubLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
