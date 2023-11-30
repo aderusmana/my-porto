a96089a3029cc6979002e9b3103ffda7dfee9919
aderusmana <aderusmana777@gmail.com>
2023-11-30 18:18:18 +0700

    Fix form

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import ProjectCard from './../../components/ProjectCard';
 import { motion } from 'framer-motion';
 import { fadeIn } from '/variants';
